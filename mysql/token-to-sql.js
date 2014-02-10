57adca9caccd3a7b6a2ac7c9ecbaf5d8ab71ffbf
Rebecca Turner <turner@mikomi.org>
2014-02-09 19:35:34 -0500

    Emit strings correctly for MySQL in the case of their C-style string escapes

--- mysql/token-to-sql.js
+++ mysql/token-to-sql.js
@@ -12,6 +12,29 @@ module.exports = function (dialect) {
         else if (token.type === '$commentCstyle') {
             return '/*'+token.value+'*/';
         }
+        else if (token.type === '$string') {
+            var value = "'" + token.value.replace("'","''") + "'";
+            value = value.replace(/\\(?![^%_])/,'\\\\');
+            value = value.replace(String.fromCharCode(0), '\\0');
+            value = value.replace(String.fromCharCode(8), '\\b');
+            value = value.replace(String.fromCharCode(10), '\\n');
+            value = value.replace(String.fromCharCode(13), '\\r');
+            value = value.replace(String.fromCharCode(9), '\\t');
+            value = value.replace(String.fromCharCode(26), '\\Z');
+            if (token.type == '$bstring') {
+                return 'B' + value;
+            }
+            else if (token.type == '$xstring') {
+                return 'X' + value;
+            }
+            else if (token.type == '$nstring') {
+                return 'N' + value;
+            }
+            else if (token.attr.charset) {
+                return '_' + token.attr.charset + value;
+            }
+            return value;
+        }
         else if (token.type === '$setdelimiter') {
             return 'DELIMITER ' + token.value;
         }
