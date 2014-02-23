bc187d908fdb33a15b13527982e2eb31f65210c0
Rebecca Turner <turner@mikomi.org>
2014-02-23 16:36:29 -0500

    Add SQLite specific keywords

--- sqlite.js
+++ sqlite.js
@@ -4,7 +4,7 @@ var SQLite = module.exports = {};
 SQLite.TokenMatcherL0 = require('./sql92/token-matcher-L0.js')(SQLite);
 SQLite.TokenMatcherL1 = require('./sqlite/token-matcher-L1.js')(SQLite);
 SQLite.Lex            = require('./sql92/lexer.js')(SQLite);
-SQLite.keyword        = require('./sql92/keywords.js')(SQLite);
+SQLite.keyword        = require('./sqlite/keywords.js')(SQLite);
 SQLite.toSQL          = require('./sql92/token-to-sql.js')(SQLite);
 SQLite.Colorize       = require('./sqlite/colorize.js')(SQLite);
 SQLite.CoalesceTokens = require('./sql92/coalesce-tokens.js')(SQLite);
